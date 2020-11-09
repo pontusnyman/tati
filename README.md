5376fea2820d39a2e14fe8c620fb4d759a429aea
Pontus Nyman <pontus.nyman@neotechnology.com>
2020-11-09 10:44:11 +0100

    Lets test

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # tati
 Test Automation Technical Interview
+
+This is a repo focused for automation engineers in this dark times when you cannot meet. Lets give the interviewee this repo to be able to show his automation skills.
+
+Lets go!
+
+Requirement:
+Node version ^12
+
+Setup:
+Install: npm i
+Start test: npm test
---
+++ testscafe/test.spec.js
@@ -0,0 +1,19 @@
+/* global fixture */
+
+import { Selector } from 'testcafe'
+
+fixture `Lets go to SwagLabs and test`
+  .page `https://www.saucedemo.com/`
+
+
+// Run it and correect this test
+test('Check the title', async t => {
+  await t.expect(Selector('title').innerText).eql('hi')
+});
+
+// Use the locked_out_user and check if you get a error msg
+test.skip('', async t =>  {})
+// Use problem_user and buy something, check if the cart is empyt after, and then logout
+test.skip('', async t =>  {})
+// Use standard_user and buy a product, and then logout
+test.skip('', async t =>  {})
