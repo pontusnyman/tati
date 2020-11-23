9ccc6e5743af736348164c4a4fc9d6e80127543f
Mumtaz <mumtaz.maqsood@yahoo.com>
2020-11-23 14:40:42 +0100

    TEST-CASE-3--> Check Cart is not empty with 'problem_user' code has written

--- testscafe/test.spec.js
+++ testscafe/test.spec.js
@@ -1,14 +1,53 @@
 /* global fixture */
 
-import { Selector } from 'testcafe'
+import { Selector, ClientFunction } from 'testcafe'
 
-
+const getWindowLocation = ClientFunction(() => window.location);
 
 //------Locators--->LOGIN PAGE-----------------------------------------------------------
 const userName = Selector('#user-name');
 const userPassword = Selector('#password')
 const loginBtn = Selector('#login-button')
 //----------------------------------------------------------------------------------------
+
+//------Locators--->INVENTORY PAGE--------------------------------------------------------
+const addToCart_btn = Selector(() => {
+  return document.getElementsByClassName('btn_primary btn_inventory')
+});
+const shoppingCart_btn = Selector('.shopping_cart_container')
+//----------------------------------------------------------------------------------------
+
+//------Locators--->CART PAGE-------------------------------------------------------------
+const checkoutBtn = Selector(() => {
+  return document.getElementsByClassName('btn_action checkout_button')
+});
+
+let cartItems = ''
+    cartItems = Selector(() => {
+        return document.getElementsByClassName('cart_item')
+});
+//----------------------------------------------------------------------------------------
+
+//------Locators--->CHECKOUT-STEP-ONE-PAGE-------------------------------------------------
+const checkout_continoue = Selector(() => {
+  return document.getElementsByClassName('btn_primary cart_button')
+});
+const checkOut_firstName = Selector('#first-name')
+const checkout_lastName = Selector('#last-name')
+const checkout_zip = Selector('#postal-code')
+//----------------------------------------------------------------------------------------
+
+//------Locators--->CHECKOUT-STEP-TWO-PAGE------------------------------------------------
+const checkoutFinsh_btn = Selector(() => {
+  return document.getElementsByClassName('btn_action cart_button')
+});
+//----------------------------------------------------------------------------------------
+
+//------Locators--->CHECKOUT-COMPLETE-PAGE------------------------------------------------
+const logOut_btn = Selector('.bm-burger-button')
+const logout = Selector('#logout_sidebar_link')
+//----------------------------------------------------------------------------------------
+
 
 
 fixture `Lets go to SwagLabs and test`
@@ -34,6 +73,13 @@ test('TEST-CASE-1->Check the title\n--------------------------------------------
 
 //------TEST-CASE-2--> Check locket_out_user and Get error msg----------------------------
 // Use the locked_out_user and check if you get a error msg
+/*
+Test Case
+  1- Open URL 'https://www.saucedemo.com/'
+  2- Enter userName: 'locket_out_user' & password:'secret_sauce' 
+  3- Click on 'Login' button
+  4- Error message should be displayed 
+*/
 //----------------------------------------------------------------------------------------
 test('TEST-CASE-2->Check locket_out_user and Get error msg \n-----------------------------------------------------------------------------', async t =>  {
   await t
@@ -51,9 +97,60 @@ test('TEST-CASE-2->Check locket_out_user and Get error msg \n-------------------
 //------END TEST-CASE-2--> Check locket_out_user and Get error msg-------------------------
 
 
-
+//------TEST-CASE-3--> Check Cart is not empty with 'problem_user'------------------------
 // Use problem_user and buy something, check if the cart is empyt after, and then logout
-test.skip('', async t =>  {})
+/*
+Test Case
+  1- Open URL 'https://www.saucedemo.com/'
+  2- Enter userName: 'problem_user' & password:'secret_sauce' 
+  3- Click on 'Login' button
+  4- Page redirect to 'Inventory.html' page, add item in cart
+  5- Click on 'Shopping Cart' icon --> Page redirect to 'Cart' page
+  6- Click on 'CheckOut' button, Page redirect then add credientials & click on 'Continue' Button
+  7- Page redirected, click on 'Finish' button and then click on again 'shopping cart' button
+  8- Items are still present in shopping cart
+  9- Store 'Page Location' Details and Count Items in Cart, then click on 'menu' upper top left and logout
+  10- Generate report
+*/ 
+//----------------------------------------------------------------------------------------
+test('TEST-CASE-3->Check Cart is not empty\n-----------------------------------------------------------------------------', async t =>  {  
+  await t
+  .typeText(userName, 'problem_user', {speed:0.1})
+  .typeText(userPassword, 'secret_sauce', {speed:0.1})
+  .click(loginBtn)
+  .click(addToCart_btn)
+  .click(shoppingCart_btn)
+  .click(checkoutBtn)
+  .wait(2000)
+  .typeText(checkOut_firstName, 'mumtaz')
+  .typeText(checkout_lastName, 'maqsood')
+  .typeText(checkout_zip, '2860')
+  .click(checkout_continoue)
+  .click(checkoutFinsh_btn)
+  .wait(2000)
+  .click(shoppingCart_btn)
+  const pageLocation = await getWindowLocation();  //get page location and store in location variable
+  const itemsinCart = await cartItems.count;
+  await t
+  .click(logOut_btn)
+  .wait(2000)
+  .click(logout)
+  console.log("-----------------------------------------------------------------------------")
+  console.log("TEST-CASE-3->Check Cart is not empty-->RESULT-->FAIL")
+  console.log("-----------------------------------------------------------------------------")
+  console.log("Expected Result--> Cart should be empty after purchasing with user 'problem_user'")
+  console.log("Actual Result---> Cart is not empty after purchasing--> Items in cart present:", itemsinCart)
+  console.log("Bug Type---> Functional\nPriority---> URGENT \n Sverity---> CRITICAL")
+  console.log("---------------------------PAGE INFO-----------------------------------------\n")
+  console.log("href:",pageLocation.href,"\n", "Origin:",pageLocation.origin, "\n", "PathName:",pageLocation.pathname,"\n","Protocol:", pageLocation.protocol)
+  console.log("-----------------------------------------------------------------------------")
+  
+});
+//----------END TEST-CASE-3->Check Cart is not empty -------------------------------------------
+
+
+
+
 // Use standard_user and buy a product, and then logout
 test.skip('', async t =>  {})
 // BONUS: Use problem_user and see if all images render properly
