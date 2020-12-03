3ac43be6c686abe3a901506c271d716e14691fa6
Pontus Nyman <17062233+pontusnyman@users.noreply.github.com>
2020-12-03 14:18:13 +0100

    Revert "Finished Task (#3)" (#8)
    This reverts commit 8c08c8ec4cee2a3faa73f403429585d29b27a313.

--- testscafe/test.spec.js
+++ testscafe/test.spec.js
@@ -1,63 +1,21 @@
 /* global fixture */
 
-import PageCommons from './PageModels/page-commons';
-import LoginPage from './PageModels/login-page';
-import InventoryPage from './PageModels/inventory-page';
-import CartPage from './PageModels/cart-page';
-import CheckoutPage from './PageModels/checkout-page';
+import { Selector } from 'testcafe'
 
-fixture`Lets go to SwagLabs and test`.page`https://www.saucedemo.com/`;
+fixture `Lets go to SwagLabs and test`
+  .page `https://www.saucedemo.com/`
 
-// // Run it and correect this test
-test('Check the title', async (t) => {
-  await t.expect(LoginPage.getTitle().innerText).eql('Swag Labs');
+
+// Run it and correect this test
+test('Check the title', async t => {
+  await t.expect(Selector('title').innerText).eql('hi')
 });
+
 // Use the locked_out_user and check if you get a error msg
-test('Attempt login as locked_out_user and check for error msg', async (t) => {
-  await LoginPage.userLogin('locked_out_user');
-  await t
-    .expect(LoginPage.getErrorMessage().innerText)
-    .eql('Epic sadface: Sorry, this user has been locked out.');
-});
+test.skip('', async t =>  {})
 // Use problem_user and buy something, check if the cart is empyt after, and then logout
-test('Buy something as problem_user, check if cart is empty, logout', async (t) => {
-  await LoginPage.userLogin('problem_user');
-  await InventoryPage.addProductToCart(4);
-  await PageCommons.goToCartOverview();
-  await CartPage.goToCheckout();
-  await CheckoutPage.checkoutInformation('John', 'Doe', '123-3456');
-  await CheckoutPage.continueCheckout();
-  await CheckoutPage.finishCheckout();
-  await t
-    .expect(CheckoutPage.getCheckoutHeader().innerText)
-    .eql('THANK YOU FOR YOUR ORDER');
-  await t.expect(PageCommons.getShoppingCartBadge().innerText).notEql('1');
-  await PageCommons.toggleHamburgerMenu();
-  await PageCommons.logout();
-});
+test.skip('', async t =>  {})
 // Use standard_user and buy a product, and then logout
-test('Buy a product as standard_user and then logout', async (t) => {
-  await LoginPage.userLogin('standard_user');
-  await InventoryPage.addProductToCart(4);
-  await PageCommons.goToCartOverview();
-  await CartPage.goToCheckout();
-  await CheckoutPage.checkoutInformation('David', 'Smith', '123-3456');
-  await CheckoutPage.continueCheckout();
-  await CheckoutPage.finishCheckout();
-  await t
-    .expect(CheckoutPage.getCheckoutHeader().innerText)
-    .eql('THANK YOU FOR YOUR ORDER');
-  await PageCommons.toggleHamburgerMenu();
-  await PageCommons.logout();
-  await t.expect(LoginPage.getTitle().innerText).eql('Swag Labs');
-});
+test.skip('', async t =>  {})
 // BONUS: Use problem_user and see if all images render properly
-test('As problem_user see if all images are correctly rendered', async (t) => {
-  await LoginPage.userLogin('problem_user');
-  const imageCount = await InventoryPage.getAllInventoryImgs().count;
-  for (var i = 0; i < imageCount; i++) {
-    await t
-      .expect(InventoryPage.getAllInventoryImgs().nth(i).getAttribute('src'))
-      .notContains('jpgWithGarbageOnItToBreakTheUrl', 'Image is broken');
-  }
-});
+test.skip('', async t =>  {})
