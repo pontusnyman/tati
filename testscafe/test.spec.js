943d1eba3623b2d4917af51c009cc0c07108b41a
Pontus Nyman <17062233+pontusnyman@users.noreply.github.com>
2021-02-08 10:09:54 +0100

    Update test.spec.js

--- testscafe/test.spec.js
+++ testscafe/test.spec.js
@@ -2,20 +2,47 @@
 
 import { Selector } from 'testcafe'
 
+/* The fixture will go to the correct website
+at the login page you will find all the different users and pw for 
+the assigments, Good luck! */
 fixture `Lets go to SwagLabs and test`
   .page `https://www.saucedemo.com/`
 
-
-// Run it and correect this test
+/* Run it and correect this test */
 test('Check the title', async t => {
   await t.expect(Selector('title').innerText).eql('hi')
 });
 
-// Use the locked_out_user and check if you get a error msg
+/* Use the locked_out_user and check if you get a error msg */
 test.skip('', async t =>  {})
-// Use problem_user and buy something, check if the cart is empyt after, and then logout
+
+/* Login with problem_user
+ and add the 'Sauce Labs Onesie' item to the cart,
+ Go through the buy process,
+ When the item is bought check if the cart is empty after,
+ and then logout (HINT: test should fail) */
 test.skip('', async t =>  {})
-// Use standard_user and buy a product, and then logout
+
+/* Login with standard_user
+ change the sorting of products 
+ to 'Price (Low to High)'
+ verify if its correct */
 test.skip('', async t =>  {})
-// BONUS: Use problem_user and see if all images render properly
+
+/* Login with standard_user
+ and add the 'Sauce Labs Onesie' item to the cart,
+ Go through the buy process,
+ When the item is bought check if the cart is empty after,
+ and then logout */
+test.skip('', async t =>  {})
+
+/* BONUS 1: Use problem_user and see if all
+images render properly. (Hint: test should fail */
+test.skip('', async t =>  {})
+
+/* BONUS 2: Use performance_glitch_user
+and verify that the website have good performance.
+(Hint: set a threshold, test should fail with
+performance_glitch_user and it should succeed 
+with standard_user */
 test.skip('', async t =>  {})
